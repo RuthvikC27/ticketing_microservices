b19a883efd548c840de6689e29f79d0c0920912a
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-08 23:06:09 +0530

    Initial

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 // ROUTES
 import { currentUserRouter } from './routes/current-user';
@@ -17,10 +19,24 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
-app.use('*', () => {
+app.use('*', async() => {
     throw new NotFoundError();
 })
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("Listening on port 3000!"))
+const start = async() => {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(() => console.log("DB connected!"))
+    .catch(err => console.log(err))
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000!")
+    })
+}
+
+start();
+
