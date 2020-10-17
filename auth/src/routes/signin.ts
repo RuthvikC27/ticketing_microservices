a9e087fab1ffcd4986fc679c5796cf251a9bd592
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-18 02:59:05 +0530

    Initial

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validationRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
+router.post("/api/users/signin", [
+    body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+    body('password')
+    .trim()
+    .notEmpty()
+    .withMessage("You must supply a password")
+], 
+validationRequest,
+(req: Request, res: Response) => {
+    
     res.send("signin");
 })
 
