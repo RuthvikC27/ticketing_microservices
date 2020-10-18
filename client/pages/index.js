642cccbcdfce206bb9d23bb5468cb3721191f36a
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-18 21:09:40 +0530

    Initial

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <h1>Heading</h1>
     </div>
   )
 }
+
+export default  Home
+
+// hot-dev-client.js?647e:126 ./pages/index.js
+// Anonymous arrow functions cause Fast Refresh to not preserve local component state.
+// Please add a name to your function, for example:
+
+// Before
+// export default () => <div />;
+
+// After
+// const Named = () => <div />;
+// export default Named;
+
+// A codemod is available to fix the most common cases: https://nextjs.link/codemod-ndc
