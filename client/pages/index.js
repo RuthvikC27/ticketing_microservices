4edfe8ff2adc4b7091f2841c57b1ead907c6d494
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-18 20:49:37 +0530

    Initial

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,10 @@
-export default () => {
-    return <div>
-        Index
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Heading</h1>
     </div>
+  )
 }
