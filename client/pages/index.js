e6bc9b2682d4bb74c8050881058674b434c7f048
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-20 13:45:08 +0530

    Initial

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 import buildClient from '../api/build-client';
 
  function Home ( { currentUser } ) {
-   console.log(currentUser);
+
   return (
     <div>
-      <h1>Heading </h1>
+      {currentUser ? <h1>You are signed in </h1> : <h1> you are NOT signed in </h1>}
     </div>
   )
 }
 
 Home.getInitialProps = async (context) => {
+
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser")
 
