9eeb0e3b782636a20ce5098f472fbbe793d600e8
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-21 10:56:47 +0530

    Initial

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -38,7 +38,11 @@ const start = () => {
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined!");
     }
-    mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+
+    if(!process.env.MONGO_URI){
+        throw new Error("JMONGO_URI must be defined!");
+    }
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
