5983b2094b94d507099f6741e604645b9ca50781
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-21 18:56:27 +0530

    Initial

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-// ROUTES
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-
-import { errorHandler, NotFoundError } from '@rc27tickets/common';
-
-const app = express();
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.use('*', async() => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = () => {
     if(!process.env.JWT_KEY){
