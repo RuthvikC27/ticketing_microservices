6c78e8aabedec7bdbdae96a2fd7d563050bdb55d
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-10-27 11:57:21 +0530

    Initial

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,28 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-
-import { errorHandler, NotFoundError } from '@rc27tickets/common';
-
-const app = express();
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use('*', async() => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = () => {
     if(!process.env.JWT_KEY){
